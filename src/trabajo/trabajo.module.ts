d02b15be38ef1dde16939c93576b231bbdbe1f63
cristopher <cristophercc67@gmail.com>
2024-11-14 23:51:03 -0300

    correción de importación de dependencias

--- src/trabajo/trabajo.module.ts
+++ src/trabajo/trabajo.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TrabajoService } from './trabajo.service';
 import { TrabajoController } from './trabajo.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [TrabajoService],
+  providers: [TrabajoService, PrismaService],
   controllers: [TrabajoController]
 })
 export class TrabajoModule {}
