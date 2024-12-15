6a1b693ecee5280f6f7affeb7c247d36c8a8d516
cristopher <cristophercc67@gmail.com>
2024-12-14 21:17:33 -0300

    welcome message

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { CuidadoresModule } from './cuidadores/cuidadores.module';
 import { MascotasModule } from './mascotas/mascotas.module';
 import { TrabajoModule } from './trabajo/trabajo.module';
+import { welcome } from './welcome.controller';
 
 @Module({
   imports: [
@@ -9,5 +10,6 @@ import { TrabajoModule } from './trabajo/trabajo.module';
     MascotasModule,
     TrabajoModule
   ],
+  controllers:[welcome]
 })
 export class AppModule {}
